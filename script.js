c22fcaf788a88c63fbb6fd161333a6a0323f0e0e
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-05-22 22:06:56 +0200

    more complex schema

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log('Conne
 
 run();
 async function run() {
-  const user = await User.create({ name: 'Daniel', age: 23 })  
-  user.name = 'Mela';
-  await user.save();
+  const user = await User.create({
+     name: 'Daniel',
+      age: 23,
+      hobbies: ['Coding', 'Gaming'],
+      address: {
+        street: '123 Fake St',
+        city: 'Springfield',
+      }
+     })  
+  // user.name = 'Mela';
+  // await user.save();
   console.log(user);
 }
--- user.js
+++ user.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String
+})
+
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number
+  age: Number,
+  email: String,
+  createdAt: Date,
+  updatedAt: Date,
+  bestFriend: mongoose.Schema.Types.ObjectId,
+  hobbies: [String],
+  address: addressSchema
 })
 
 module.exports = mongoose.model('User', userSchema);
