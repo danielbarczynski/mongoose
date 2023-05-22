0dd0a96c00cb04c88fb3a5359b3adbeec53262bf
Daniel Barczyński <daniel.barczynski@gearcode.eu>
2023-05-22 19:08:12 +0200

    fixed connection

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/');
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => console.log('Connected to MongoDB'));
+
